5bd569aad1c017413edba2efc19f75ad3fc20a0d
Moiz Zulfiqar <moiz.zulfiqar1991@gmail.com>
2017-03-20 17:22:08 +0500

    Revert "- Cache Project NodeId Fix"
    This reverts commit 84c9575075fa2ee2eef8430966114df48e901d73.

--- src/TPCTrainco.Umbraco.Extensions/Models/CourseDetail.cs
+++ src/TPCTrainco.Umbraco.Extensions/Models/CourseDetail.cs
@@ -29,12 +29,10 @@ namespace TPCTrainco.Umbraco.Extensions.Models
         [NullSetting(NullSetting = NullSettings.Null)]
         public string DetailsUrl { get; set; }
 
-        //[NullSetting(NullSetting = NullSettings.Null)]
-        //[Ignore]
-        //public int NodeId { get; set; }
+        [NullSetting(NullSetting = NullSettings.Null)]
+        public int NodeId { get; set; }
 
         public double Price { get; set; }
-
 
         
     }
